4d81700175226af6dbca5868b789d8eae2633a13
rshmnkgit <64658801+rshmnkgit@users.noreply.github.com>
2020-08-09 22:09:21 -0400

    config file updated

--- EmployeeSQL/config.py
+++ EmployeeSQL/config.py
@@ -1,6 +1,5 @@
 # Add your postgres username and password
 username = "POSTGRES USERNAME HERE"
 password = "POSTGRES PASSWORD HERE"
-ipaddress = "YOUR IP Address Here"
 port = "YOUR PORT HERE"
 dbname = "YOUR DATABASE NAME HERE"
